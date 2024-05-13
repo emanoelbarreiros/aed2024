e22ba2b6f0e941090d9d0b58fd3b576c05734081
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2024-05-12 21:26:49 -0300

    mergesort

--- src/algoritmos/TesteMergeSort.java
+++ src/algoritmos/TesteMergeSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TesteMergeSort {
 
     public static void main(String[] args) {
-        int[] v = {5,1,2,3,7,4,0};
+        int[] v = {5,1,2,3,7,4,0,6};
 
         System.out.println(Arrays.toString(v));
 
